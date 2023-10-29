56a4ffa9fb0ced6f03d28b5776264d9e48bbd6e9
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-10-29 19:54:44 -0300

    first commit

--- lib/cloudfront-website-stack.ts
+++ lib/cloudfront-website-stack.ts
@@ -1,16 +1,85 @@
 import * as cdk from 'aws-cdk-lib';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CanonicalUserPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { ARecord, CfnRecordSet, HostedZone, RecordTarget, RecordType } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class CloudfrontWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "Bucket", {
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudfrontWebsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    new BucketDeployment(this, "BucketDeployment", {
+      destinationBucket: bucket,
+      sources: [Source.asset(join(__dirname, "/website"))],
+    });
+
+    const cfOAI = new OriginAccessIdentity(this, "OriginAccessIdentity");
+
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new CanonicalUserPrincipal(cfOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)
+        ]
+      })
+    );
+
+    const certificateArn = process.env.CERTIFICATE_ARN!;
+    const certificate = Certificate.fromCertificateArn(this, "DomainCertificate", certificateArn);
+
+    const recordName = "site";
+    const domainName = "lazinessdevs.com";
+
+    const distribution = new Distribution(this, "Distribution", {
+      certificate: certificate,
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {
+          originAccessIdentity: cfOAI,
+        }),
+      },
+      domainNames: [[recordName, domainName].join(".")],
+      defaultRootObject: "index.html",
+    });
+
+    const hostedZoneId = process.env.HOSTED_ZONE_ID!;
+
+    // const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
+    //   hostedZoneId: hostedZoneId,
+    //   zoneName: domainName,
     // });
+
+    // new ARecord(this, "ARecord", {
+    //   recordName: recordName,
+    //   zone: hostedZone,
+    //   target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    // });
+
+    new CfnRecordSet(this, "ARecord", {
+      hostedZoneId: hostedZoneId,
+      type: "A",
+      name: [recordName, domainName].join("."),
+      aliasTarget: {
+        dnsName: distribution.distributionDomainName,
+        // CloudFront distribution - Specify Z2FDTNDATAQYW2 .
+        // This is always the hosted zone ID when you create an alias record that routes traffic to a CloudFront distribution.
+        hostedZoneId: "Z2FDTNDATAQYW2",
+        evaluateTargetHealth: false,
+      }
+    })
   }
 }
